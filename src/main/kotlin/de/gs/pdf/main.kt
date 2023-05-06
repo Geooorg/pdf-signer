b5c64d199a9117def522e755dab60fe1ab7629c2
Georg Stach <info@GeorgStach.de>
2023-05-07 00:34:50 +0200

    Replaced iText against PDFBox

--- src/main/kotlin/de/gs/pdf/main.kt
+++ src/main/kotlin/de/gs/pdf/main.kt
@@ -4,10 +4,8 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.help
 import com.github.ajalt.clikt.parameters.arguments.multiple
-import com.github.ajalt.clikt.parameters.arguments.optional
 import com.github.ajalt.clikt.parameters.types.file
-import de.gs.pdf.service.PdfFileReader
-import de.gs.pdf.service.SignService
+import de.gs.pdf.service.PdfFileWriter
 import java.util.*
 import kotlin.system.exitProcess
 
@@ -17,21 +15,25 @@ class Main : CliktCommand() {
 //    val files: String by option(help = "file list to sign").prompt("files")
 //    private val files: List<String> by argument().multiple()
     val code by argument().help("Text to add as code ")
+    val author by argument().help("Your name")
     val files by argument().file(mustExist = true).multiple().help("list of files to sign")
+
     override fun run() {
 
-        val fileList = files.filter { f -> f.name.endsWith(".pdf", true) }
-        val pdfReader = PdfFileReader(fileList, code)
-        pdfReader.readFiles()
+        val uuid = UUID.randomUUID()
+        println(" Generated UUID is: $uuid")
 
-        val signService = SignService()
+        val fileList = files.filter { f -> f.name.endsWith(".pdf", true) }
+        val pdfReader = PdfFileWriter(fileList, code, author, uuid)
+        pdfReader.addTextSignature()
+
+        // val signService = SignService()
     }
 
 }
 
 fun main(args: Array<String>) {
-    val generatedUUID = UUID.randomUUID()
-    println("Welcome to PDF Signer. Today's UUID is: ")
+    println("Welcome to PDF Signer")
 
     if (args.isEmpty()) {
         println("Please pass your certificate as first argument and the file(s) to sign")
---
+++ src/main/kotlin/de/gs/pdf/service/PdfFileWriter.kt
@@ -0,0 +1,60 @@
+package de.gs.pdf.service
+
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.PDPage
+import org.apache.pdfbox.pdmodel.PDPageContentStream
+import org.apache.pdfbox.pdmodel.common.PDRectangle
+import org.apache.pdfbox.pdmodel.font.PDType1Font
+import java.io.File
+import java.io.IOException
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.UUID
+
+
+class PdfFileWriter(val fileNames: List<File>, val code: String, val author: String, val uuid: UUID) {
+    fun addTextSignature() {
+        fileNames.forEach { file ->
+
+            val outFile = destinationFileName(file)
+            val dateTime = formattedDateTime()
+
+            try {
+                PDDocument.load(File(file.absolutePath)).use { document ->
+
+                    val page = PDPage(PDRectangle.A4)
+                    document.addPage(page)
+
+                    PDPageContentStream(document, page).use { contentStream ->
+
+                        contentStream.beginText()
+                        contentStream.setFont(PDType1Font.TIMES_ROMAN, 10f)
+                        contentStream.newLineAtOffset(50f, 700f)
+                        contentStream.showText("Signatur: Code '$code' um ${dateTime}, UUID: $uuid")
+                        contentStream.endText()
+
+                        contentStream.beginText()
+                        contentStream.setFont(PDType1Font.TIMES_ROMAN, 12f)
+                        contentStream.newLineAtOffset(50f, 680f)
+                        contentStream.showText(author)
+                        contentStream.endText()
+                    }
+
+                    document.save(outFile.absolutePath)
+                    println("${outFile.name} written successfully.")
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun destinationFileName(file: File) =
+            File(file.path.replace(file.name, ""), file.name.replace(".pdf", "") + "_signed.pdf")
+
+    private fun formattedDateTime(): String {
+        val now = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm:ss")
+        return now.format(formatter)!!
+    }
+}
